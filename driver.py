217a22761eaaf7a66f6d4f63eaa997999ac2d4d0
ibagayoko <46138891+ibagayoko@users.noreply.github.com>
2019-02-23 16:05:44 -0400

    Update driver.py
    fix bugs

--- driver.py
+++ driver.py
@@ -7,18 +7,24 @@ class Driver(object):
     
     for index, interval in enumerate(intervals):
       results = [module.getResult(interval) for module in self._modules] 
-      votes = defaultdict(int)
+      from collections import defaultdict
+      votes = dict()
       for res in results:
-        votes[res]+=1
+        try:
+          votes[res]+=1
+          
+        except:
+          votes[res] = 0
+          
       import operator
-      winner  = max(stats.iteritems(), key=operator.itemgetter(1))[0]
+      winner  = max(votes.items(), key=operator.itemgetter(1))[0]
       self._values.append(winner)
       self._intervals.append(interval)
       for i, module in enumerate(self._modules):
         if results[i]==winner:
-          module.receiveFeedback({status:'ok'})
+          module.receiveFeedback({'status':'ok'})
         else:
-          module.receiveFeedback({status:'error', goodvalue:winner})
+          module.receiveFeedback({'status':'error', good_value:winner})
         
       
     
