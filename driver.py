e16b85d5979eb023deca03275b2388f4160dec49
ibagayoko <46138891+ibagayoko@users.noreply.github.com>
2019-02-23 16:33:46 -0400

    Update driver.py
    print for test purpose

--- driver.py
+++ driver.py
@@ -3,13 +3,21 @@ class Driver(object):
     self._modules = modules
     self._intervals  = []
     self._values     = []
+    print('modules |\t', end='')
+    for i, _ in enumerate(modules):
+      print('#%d\t'.format(i), end='')
+    print('Winner')
   def test(self, intervals):
     
     for index, interval in enumerate(intervals):
       results = [module.getResult(interval) for module in self._modules] 
+      
+      
       from collections import defaultdict
       votes = dict()
+      print('result |\t', end='')
       for res in results:
+        print(str(res)+'\t', end='')
         try:
           votes[res]+=1
           
@@ -18,9 +26,11 @@ class Driver(object):
           
       import operator
       winner  = max(votes.items(), key=operator.itemgetter(1))[0]
+      print(winner)
       self._values.append(winner)
       self._intervals.append(interval)
       for i, module in enumerate(self._modules):
+        
         if results[i]==winner:
           module.receiveFeedback({'status':'ok'})
         else:
